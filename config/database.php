b793a13b5823681caf95a3fa7d60776b1083372c
unknown <minecraft79@outlook.es>
2026-07-01 17:10:02 -0500

    activacion base de datos produccion

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 
 // Detectar automáticamente el entorno
 // Usa la variable de entorno APP_ENV. Por defecto usa LOCAL
-$appEnv = getenv('APP_ENV') ?: 'ENV';
+$appEnv = getenv('APP_ENV') ?: 'PRODUCTION';
 $isProduction = strtoupper($appEnv) === 'PRODUCTION';
 
 if ($isProduction) {
